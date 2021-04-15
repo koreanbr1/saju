e951ad18f63e924e85d298609d1fd387e7a3d356
unknown <koreanbr10@gmail.com>
2021-04-15 18:58:43 +0900

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,118 @@
 <template>
-  <div id="HelloWorld">
-    성함
-    <form>
-      <input type="name" v-model="name"><br>
-    </form>
-
+<div class="wrap">
+  <div class="left">
+    <div class="title">
+      通圓書堂 體性命理
+    </div>
     <br>
-    성별(남자는 '양', 여자는 '음'으로 입력)
-    <form>
-      <input type="gender" v-model="gender"><br>
-    </form>
-    <br>
-
-    干支 입력 (연간,연지...시간,시지 순으로)
-    <br>test: 신미을미신사을미<br>
-    test: 임신갑진임자경자<br>
-    test:임자임자병술무술
-    <form>
-      <input
-        type="text"
-        placeholder="간지를 한글로 입력하세요"
-        v-model="typed_ganzi" />
-    </form>
-    <br>
-    <button @click="[change_ganzi(),cal_gisae(12),cal_5hang_cons()]">간지입력완료</button>
-    <div v-show = "ganzi[0].hanja == '辛'" class="test_line"></div>
-    <table >
-      <!-- <tr>
-        <th> 大運 </th>
-        <th v-for = "i in ['時柱','日柱','月柱','年柱']" :key="i" >{{ i }}</th>
-      </tr> -->
-      <tr>
-        <th class='big_letter'> {{big_luck[0].big_luck_sky}}<sup>{{big_luck[0].umyang}}</sup><sub>{{big_luck[0].ten_shin}}</sub> </th>
-        <th class='big_letter' v-for = "i in [6,4,2,0]" :key="i">{{ ganzi[i].hanja }}<sup>{{ganzi[i].umyang}}</sup><sub>{{ganzi[i].ten_shin}}</sub></th>
-      </tr>
-      <tr>
-        <th class='big_letter'> {{big_luck[1].big_luck_ear}} </th>
-        <th class='big_letter' v-for = "i in [7,5,9,1]" :key="i">{{ ganzi[i].hanja }}</th>
-      </tr>
-      <tr>
-        <th class='small_letter'> {{big_luck[2].big_luck_Amzang[0]}} </th>
-        <th class='small_letter' v-for = "i in [7,5,9,1]" :key="i">{{ ganzi[i].amjang[1] }}</th>
-      </tr>
-      <tr>
-        <th class='small_letter'> {{big_luck[2].big_luck_Amzang[1]}} </th>
-        <th class='small_letter' v-for = "i in [7,5,9,1]" :key="i">{{ ganzi[i].amjang[2] }}</th>
-      </tr>
-      <tr>
-        <th class='small_letter'> {{big_luck[2].big_luck_Amzang[2]}} </th>
-        <th class='small_letter' v-for = "i in [7,5,9,1]" :key="i">{{ ganzi[i].amjang[3] }}</th>
-      </tr>
-    </table>
-
-    運路(운로)
-    <button @click=[update_Bigluck(12),cal_gisae(12)]>원국만 보기</button>
-    
-    <table >
-      <tr>    
-        <td v-for = "i in [11,10,9,8,7,6,5,4,3,2,1,0]" :key="i">
-          <button @click=[update_Bigluck(i),cal_gisae(i)]>{{ get_BigLuck()[i][0][0] }}<br>
-            {{get_BigLuck()[i][0][1] }}
-          </button>
-          <br>
+    <form class="form2">
+      <div>
+          <label for="name">姓銜:</label>
+          <input type="text" v-model="name" />
+      </div>
+      <div>
+          <label for="name">生年月日時:</label>
           <select>
-            <option v-for= "option in GH" :key= "option.text">
+              <option v-for= "option in UY" :key= "option.text">
+              {{ option.text }}
+              </option>
+          </select>
+          <input type="Birth_date" v-model="Birth_date" />
+      </div>
+      <div>
+          <label for="name">性別:</label>
+          <input type="gender" v-model="gender" />
+      </div>
+      
+      <div>
+          <label for="name">干支:</label>
+          <input type="text" v-model="typed_ganzi" />
+      </div>
+      <button @click="[change_ganzi(),cal_gisae(12),cal_5hang_cons(),get_gender()]">입력완료</button>
+      <br>test: 신미을미신사을미<br>
+      test: 임신갑진임자경자<br>
+      test:임자임자병술무술
+    </form>
+    <br>
+    <form class="form2">
+      <div>
+          <label for="name">旺勢:</label>
+          <input type="text"/>
+      </div>
+      <div>
+          <label for="name">用神:</label>
+          <input type="text"/>
+      </div>
+      <div>
+          <label for="name">體性格局:</label>
+          <select>
+            <option v-for= "option in GeokGuk" :key= "option.text">
             {{ option.text }}
             </option>
           </select>
-        </td>
-      </tr>
-    </table>
-    
-    <!-- {{gisae[0].value}},{{gisae[0].content}}<br>
-    {{gisae[1].value}},{{gisae[1].content}}<br>
-    {{gisae[2].value}},{{gisae[2].content}}<br>
-    {{gisae[3].value}},{{gisae[3].content}}<br>
-    {{gisae[4].value}},{{gisae[4].content}}<br> -->
+      </div>
+    </form>
+  </div>
+  <div class="center">
+    <div class="four_pillar">
+      <div class="single_pilar" v-if= "big_luck[0].big_luck_sky != 'o' ">
+        <div class="single_letter">
+          {{ big_luck[0].big_luck_sky }}
+        </div>
+        <div class="single_letter">
+          {{ big_luck[1].big_luck_ear }}
+        </div>
+        <div class="amjang_letter" v-for= "j in [0,1] " :key="'B'+j">
+          <span class="amjang_letter_1">{{big_luck[2].big_luck_Amzang[j][0]}}</span>
+          <span class="amjang_letter_2">{{big_luck[2].big_luck_Amzang[j][1]}}</span>
+          <span class="amjang_letter_2">{{big_luck[2].big_luck_Amzang[j][2]}}</span>
+        </div>
+        <div class="amjang_letter" v-if = "big_luck[2].big_luck_Amzang.length > 2">
+          <span class="amjang_letter_1">{{big_luck[2].big_luck_Amzang[2][0]}}</span>
+          <span class="amjang_letter_2">{{big_luck[2].big_luck_Amzang[2][1]}}</span>
+          <span class="amjang_letter_2">{{big_luck[2].big_luck_Amzang[2][2]}}</span>
+        </div>
+        <div class="amjang_letter" v-else>
+          &nbsp; 
+        </div>
+      </div>
+      <div class="single_pilar" v-else>
+        <div class="single_letter">
+          &nbsp;
+        </div>
+        <div class="single_letter">
+          &nbsp;
+        </div>
+        <div class="amjang_letter" v-for= "j in [0,1,2] " :key="'B'+j">
+          &nbsp;
+        </div>    
+      </div>
+      <div class="single_pillar" v-for = "k in [0,2,4,6]" :key="k">
+        <div class="single_letter"  v-for = "i in [k,k+1]" :key="i">
+          {{ganzi[i].hanja }}
+        </div>
+        <div class="amjang_letter" v-for= "j in [1,2] " :key="'A'+j">
+          <span class="amjang_letter_1">{{ganzi[k+1].amjang[j][0]}}</span>
+          <span class="amjang_letter_2">{{ganzi[k+1].amjang[j][1]}}</span>
+          <span class="amjang_letter_2">{{ganzi[k+1].amjang[j][2]}}</span>
+        </div>
+        <div class="amjang_letter" v-if = "ganzi[k+1].amjang.length > 3">
+          <span class="amjang_letter_1">{{ganzi[k+1].amjang[3][0]}}</span>
+          <span class="amjang_letter_2">{{ganzi[k+1].amjang[3][1]}}</span>
+          <span class="amjang_letter_2">{{ganzi[k+1].amjang[3][2]}}</span>
+        </div>
+        <div class="amjang_letter" v-else>
+          &nbsp; 
+        </div>
+      </div>
+    </div>
+    <br>
     <div class="pentagon">
       <span v-for= "i in this.fhang_cons" :key=i 
       style="border: 1px solid black; padding: 5px;" 
       :class="fhang_color[i]">{{gisae[i].id }}</span>
-        <!-- <span v-for = "k in gisae[i].content" :key=k >{{gisae[i].content[k]}}</span> -->
         <span v-for = "k in this.fhang_cons" :key=k+5
         style="font-size: small">
-          <!-- <span v-for = "k in gisae[i].content" :key=k >{{gisae[i].content[k]}}</span> -->
           {{gisae[k].content[0]}}<br>
           {{gisae[k].content[1]}}<br>
           {{gisae[k].content[2]}}<br>
@@ -97,8 +126,33 @@
           {{gisae[k].value}}
           </span>
     </div>
+    <table>
+      運路(운로)
+      <button @click=[update_Bigluck(12),cal_gisae(12)]>원국만 보기</button>
+      <tr>    
+        <td v-for = "i in [11,10,9,8,7,6,5,4,3,2,1,0]" :key="i">
+          <button @click=[update_Bigluck(i),cal_gisae(i)]>
+            {{ get_BigLuck()[i][0][0] }}<br>
+            {{ get_BigLuck()[i][0][1] }}
+          </button>
+          <br>
+          <select>
+            <option v-for= "option in GH" :key= "option.text">
+            {{ option.text }}
+            </option>
+          </select>
+        </td>
+      </tr>
+    </table>
+    
     
   </div>
+  <div class="right">
+    <div class="v1">
+      {{Birth_date}}&nbsp;{{hanja_gender}}&nbsp;{{ganzi[7].hanja}}時
+    </div>
+  </div>
+ </div>
 </template>
 
 <script>
@@ -108,20 +162,22 @@ export default {
       fhang_cons: [],
       fhang_color: ["green","red","yellow","white","black"],
       typed_ganzi: '신미을미신사을미',
+      Birth_date: " ",
       ganzi: [
         {id: 0, hanja: "o", value: "o", umyang: "o", ten_shin: ""}, //연간 0
-        {id: 1, hanja: "o", value: "o", amjang: [] },//연지 1
+        {id: 1, hanja: "o", value: "o", amjang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]] },//연지 1
         {id: 2, hanja: "o", value: "o", umyang: "o", ten_shin: ""}, //월간 2
-        {id: 3, hanja: "o", value: "o", amjang: [] },//월지 3
+        {id: 3, hanja: "o", value: "o", amjang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]] },//월지 3
         {id: 4, hanja: "o", value: "o", umyang: "o", ten_shin: ""}, //일간 4
-        {id: 5, hanja: "o", value: "o", amjang: [] },//일지 5
+        {id: 5, hanja: "o", value: "o", amjang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]]  },//일지 5
         {id: 6, hanja: "o", value: "o", umyang: "o", ten_shin: ""}, //시간 6
-        {id: 7, hanja: "o", value: "o", amjang: [] },//시지 7
-        {id: 9, hanja: "o", value: "o", amjang: [] }, //월지 한번더 8
-        {id: 11, hanja: "o", value: "o", amjang: [] } //월지 한번더 9
+        {id: 7, hanja: "o", value: "o", amjang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]]  },//시지 7
+        {id: 9, hanja: "o", value: "o", amjang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]]  }, //월지 한번더 8
+        {id: 11, hanja: "o", value: "o", amjang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]]  } //월지 한번더 9
       ],
       name: '장승표',
       gender: '양',
+      hanja_gender:  '성별',
       gisae:[
         {id: '木', value: 0, content: [],id_n: 0},
         {id: '火', value: 0, content: [],id_n: 1},
@@ -168,19 +224,35 @@ export default {
           '己辛癸',['己',18],['辛',3],['癸',9]]},
       ],
       big_luck: [
-        {big_luck_sky: '', umyang: '', ten_shin: '', value: 0},
-        {big_luck_ear: ''},
-        {big_luck_Amzang: []},
+        {big_luck_sky: 'o', umyang: '', ten_shin: '', value: 0},
+        {big_luck_ear: 'o'},
+        {big_luck_Amzang: [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]]},
       ],
       GH: [
-        { text: '??'},
-        { text: '吉' },
-        { text: '平'},
-        { text: '凶'},
+        { text: '??'},{ text: '吉' },{ text: '平'},{ text: '凶'}
+      ],
+      UY: [
+        { text: '양력'},
+        { text: '음력'},
+      ],
+      GeokGuk: [
+        { text: '曲直'},{ text: '養生'},{ text: '活人'},{ text: '秋收棟樑'},{ text: '炎上'},
+        { text: '燥土生金 成器'},{ text: '燥土生金 光彩'},{ text: '水火旣濟'},{ text: '取金'},{ text: '取水'},
+        { text: '潤下'},{ text: '稼穡'},{ text: '從革'},{ text: '洗水'}
       ]
     }
   },
   methods: {
+    get_gender(){
+      if (this.gender=='양')
+      {
+        this.hanja_gender = '乾命'
+      }
+      else if(this.gender =='음')
+      {
+        this.hanja_gender = '坤命'
+      }
+    },
     cal_5hang_cons(){
       if (this.ganzi[4].value==0){
         this.fhang_cons = [0,1,2,3,4]
@@ -351,11 +423,11 @@ export default {
     },
     update_Bigluck(i){
       if (i==12){
-        this.big_luck[0].big_luck_sky = ''
+        this.big_luck[0].big_luck_sky = 'o'
         this.big_luck[0].umyang = ''
         this.big_luck[0].ten_shin = ''
-        this.big_luck[1].big_luck_ear = ''
-        this.big_luck[2].big_luck_Amzang = ''
+        this.big_luck[1].big_luck_ear = 'o'
+        this.big_luck[2].big_luck_Amzang = [["o","o","o"],["o","o","o"],["o","o","o"],["o","o","o"]]
       }
       else{
         this.big_luck[0].big_luck_sky = this.get_BigLuck()[i][0][0]
@@ -493,126 +565,156 @@ export default {
 </script>
 
 <style>
-.test_line{
-  width: "10px"
+div.wrap{
+        width: 100%;
+    }
+    div.left {
+        width: 30%;
+        float: left;
+    }
+    div.center {
+        width: 60%;
+        float: left;
+    }
+    div.right{
+      width: 10%;
+      float: left;
+    }
+div.title{
+  text-align: center
 }
-.big_letter{
-  font-size: 150%
+form.form2 {
+    /* Just to center the form on the page */
+    margin: 0 auto;
+    width: 200px;
+    /* To see the outline of the form */
+    padding: 1em;
+    border: 1px solid #CCC;
+    border-radius: 1em;
 }
-.small_letter{
-  font-size: 80%
+div.v1 {
+  font-size: 300%;
+  writing-mode: vertical-rl;
 }
-.green{
-  background-color: green;
+div.single_letter{
+  text-align: center;
+  font-size: 200%;
+  /* display: inline-block; */
+  display: block;
+  border: 1px solid black;
+  width: 70px;
+  height: 70px;
 }
-.red{
-  background-color: red;
+div.amjang_letter{
+  text-align: center;
+  font-size: 100%;
+  border: 1px solid black;
+  width: 70px;
+  height: 25px;
+  /* display: inline-block; */
+  display: block;
 }
-.yellow{
-  background-color: yellow;
+
+span.amjang_letter_1{
+  font-size: 80%;
 }
-.white{
-  background-color: white;
+span.amjang_letter_2{
+  font-size: 20%;
 }
-.black{
-  color: white;
-  background-color: black;
+
+div.single_pillar{  
+  display: inline-block;
 }
-.pentagon {
-  position: relative;
+div.four_pillar {
+  flex-wrap: nowrap;
+  display: flex;
+  padding-top: 50px;
+  padding-left: 100px;
   width: 400px;
   height: 350px;
   background-color: rgb(150, 146, 146);
 }
 
+span.green{
+  background-color: green;
+}
+span.red{
+  background-color: red;
+}
+span.yellow{
+  background-color: yellow;
+}
+span.white{
+  background-color: white;
+}
+span.black{
+  color: white;
+  background-color: black;
+}
+
+
+
+div.pentagon {
+  width: 300px;
+  height: 300px;
+  background-color: rgb(150, 146, 146);
+}
+
 .pentagon span:nth-child(1){
-  position: absolute;
-  top: 80px;
-  left: 170px;
+  position: relative;
+  top: 50px;
+  left: 135px;
   font-weight: bold;
 }
 .pentagon span:nth-child(6) {
-  position: absolute;
-  top: 40px;
-  left: 210px;
+  position: relative;
   font-weight: bold;
 }
 .pentagon span:nth-child(11) {
-  position: absolute;
-  top: 60px;
-  left: 175px;
   font-weight: bold;
 }
 .pentagon span:nth-child(2) {
-  position: absolute;
-  top: 160px;
-  left: 260px;
+  position: relative;
+  top: 90px;
+  left: 170px;
   font-weight: bold;
 }
 .pentagon span:nth-child(7) {
-  position: absolute;
-  top: 160px;
-  left: 290px;
   font-weight: bold;
 }
 .pentagon span:nth-child(12) {
-  position: absolute;
-  top: 140px;
-  left: 260px;
   font-weight: bold;
 }
 .pentagon span:nth-child(3) {
-  position: absolute;
-  top: 260px;
-  left: 220px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(8) {
-  position: absolute;
-  top: 260px;
-  left: 260px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(13) {
-  position: absolute;
-  top: 240px;
-  left: 220px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(4) {
-  position: absolute;
-  top: 260px;
-  left: 120px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(9) {
-  position: absolute;
-  top: 260px;
-  left: 20px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(14) {
-  position: absolute;
-  top: 240px;
-  left: 120px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(5) {
-  position: absolute;
-  top: 160px;
-  left: 95px;
-  font-weight: bold;
-}
-.pentagon span:nth-child(10) {
-  position: absolute;
-  top: 160px;
+  position: relative;
+  top: 0px;
   left: 0px;
   font-weight: bold;
 }
+.pentagon span:nth-child(8) {
+  font-weight: bold;
+}
+.pentagon span:nth-child(13) {
+  font-weight: bold;
+}
+.pentagon span:nth-child(4) {
+  font-weight: bold;
+}
+.pentagon span:nth-child(9) {
+  font-weight: bold;
+}
+.pentagon span:nth-child(14) {
+
+  font-weight: bold;
+}
+.pentagon span:nth-child(5) {
+  font-weight: bold;
+}
+.pentagon span:nth-child(10) {
+
+  font-weight: bold;
+}
 .pentagon span:nth-child(15) {
-  position: absolute;
-  top: 140px;
-  left: 95px;
   font-weight: bold;
 }
 /* .pentagon span:nth-child(4) {
